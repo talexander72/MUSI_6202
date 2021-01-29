6adc41baff4cb5f45fb8336eaed03c077b4ffcf3
talexander72 <78112431+talexander72@users.noreply.github.com>
2021-01-29 01:55:49 -0500

    Create correlation.py

---
+++ assignment 1/correlation.py
@@ -0,0 +1,44 @@
+### loading in packages
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sc
+from scipy.io import wavfile
+import scipy.io
+from scipy import signal
+from scipy.io.wavfile import read
+
+### function 1
+def crossCorr(x,y):
+    z = sc.correlate(x,y)
+    z = z / np.max(z)
+    return z
+    
+### function 2
+def loadSoundFile(filename):
+    fs,x = wavfile.read(filename)
+    x = x[:,0]
+    return x
+
+### QUESTION 1 MAIN FUNCTION
+def correlator():
+    snare = loadSoundFile('snare.wav')
+    loop = loadSoundFile('drum_loop.wav')
+    z = crossCorr(snare,loop)
+    n = np.arange(z.size)
+    plt.plot(n,z)
+    return
+    
+correlator()
+    
+### QUESTION 2 MAIN FUNCTION
+def findSnarePosition(snareFilename,drumloopFilename):
+    snare = loadSoundFile(snareFilename)
+    loop = loadSoundFile(drumloopFilename)
+    z = crossCorr(snare,loop)
+    guesses = []
+    for i in np.arange(len(z)):
+        if z[i]==1:
+            guesses.append(i)
+    return guesses
+
+findSnarePosition('snare.wav','drum_loop.wav')
